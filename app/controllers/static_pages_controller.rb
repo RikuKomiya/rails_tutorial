a201a2c5fac457e5b1a49dea359f6a3516b9959d
Riku Komiya <824rikukomiya@gmail.com>
2019-10-04 14:22:59 +0900

    finish HOME

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
 before_action :set_result_course_search
   def home
     @current_courses =  Course.where(sem: current_sem, wday: current_wday)
-
   end
 
   def help
@@ -42,9 +41,14 @@ before_action :set_result_course_search
 
   def set_result_course_search
 
-    @result_courses = Course.search(course_name: params[:course_name],
+    result_courses = Course.search(course_name: params[:course_name],
                                     professor: params[:professor],sem:params[:sem],
                                     wday: params[:wday],period: params[:period])
+    if result_courses.present?
+    @result_courses =result_courses.paginate(page: params[:page], per_page: 12)
+    else
+      @result_courses = nil
+    end
 
   end
 
